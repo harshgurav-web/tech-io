597834da2f09333315f55272f4bf1af7e29518a7
harshgurav-web <harshbg195@gmail.com>
2026-07-22 19:42:18 +0530

    added auths clerk and all

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,20 @@
 import dotenv from "dotenv";
-dotenv.config();
+dotenv.config({quiet: true});
 
 import app from "./src/app.js";
+import connectDB from "./src/lib/db.js";
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+const startServer = async () => {
+    try {
+        await connectDB();
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    } catch (err) {
+        console.error("Server failed to start:", err);
+    }
+};
+
+startServer();
