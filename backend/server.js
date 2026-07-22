83063083165c4949aa29daece2a51d11d7fa82bd
harshgurav-web <harshbg195@gmail.com>
2026-07-22 23:48:28 +0530

    data added here recover

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import app from "./src/app.js";
+import connectDB from "./src/config/db.js";
 
-app.listen(3000, () => {
-    console.log(`Server is running on port 3000`);
+const PORT = process.env.PORT || 3000;
 
-});                                                             
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+    connectDB();
+});
