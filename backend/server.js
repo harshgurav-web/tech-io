5acf0787c9cc98260258c11e91a1fae698f08b6d
harshgurav-web <harshbg195@gmail.com>
2026-07-23 22:23:54 +0530

    123

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,6 @@ import connectDB from "./src/config/db.js";
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log("SERVER VERSION 123");
     connectDB();
 });
